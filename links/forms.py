c15b782930e5ddd04d8bd3e9dcb96eaa71d1298b
mickey-40 <mickeyarnold53@yahoo.com>
2024-10-12 11:13:08 -0500

    finished lesson and link shortener app

--- links/forms.py
+++ links/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 
-class LinkForm(forms.Form):
-    name = forms.CharField(max_length=50)
-    url = forms.URLField(max_length=200)
-    slug = forms.SlugField(required=False)
+from .models import Link
+
+class LinkForm(forms.ModelForm):
+    class Meta:
+        model = Link
+        fields = ['name', 'url', 'slug']
--- links/views.py
+++ links/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import get_object_or_404,render,redirect
+from django.urls import reverse
 
 from .models import Link
 from .forms import LinkForm
 
 # Create your views here.
 def index(request):
-        links = Link.objects.all()
+        links = Link.objects.all().order_by("-clicks")
         context = {
                 "links" : links
         }
@@ -21,7 +22,14 @@ def root_link(request, link_slug):
         return redirect(link.url)
 
 def add_link(request):
-        form = LinkForm()
+        if request.method == 'POST':
+                form = LinkForm(request.POST)
+                if form.is_valid():
+                        #save data and return to homepage
+                        form.save()
+                        return redirect(reverse('home'))
+        else:
+                form = LinkForm()
         context = {
             "form": form 
         }
